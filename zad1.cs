aff6bcf6e68b6e77c8b54fd962f8476005a69cc5
Roman Khussein <romanfarri@gmail.com>
2023-11-15 09:17:37 +0200

    ald

---
+++ zad1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		for (int i = 1; i <= n; i++)
+		{
+			string line = "";
+			for (int j = 1; j <= n - i; j++)
+				line += "  ";
+
+			for (int j = n - i + 1; j <= n; j++)
+			{
+				if (j == n - i + 1 || j == n)
+					line += "* ";
+				else line += "+ ";
+			}
+			string newLine = line.Remove(2 * n - 1);
+			Console.WriteLine(newLine);
+			
+		}
+		for (int i = n - 1; i >= 0; i--)
+		{
+			string line = "";
+			for (int j = 1; j <= n - i; j++)
+				line += "  ";
+
+			for (int j = n - i + 1; j <= n; j++)
+			{
+				if (j == n - i + 1 || j == n)
+					line += "* ";
+				else line += "+ ";
+			}
+			string newLine = line.Remove(2 * n - 1);
+			Console.WriteLine(newLine);
+		}
+
+		// changes
+		Console.WriteLine();
+
+	}
+}
